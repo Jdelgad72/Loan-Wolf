5a5633a9c11716df268647643e605f713890ea7f
Felipe Garcilazo <feligarc@iu.edu>
2021-01-27 12:40:34 -0500

    Updated version. ERD fully translated.

--- database/table_design.sql
+++ database/table_design.sql
@@ -1,3 +1,4 @@
+/*Strong Entities*/
 CREATE TABLE user (
 userID INT(10) AUTO_INCREMENT,
 firstName VARCHAR(64) NOT NULL,
@@ -23,18 +24,24 @@ ENGINE=InnoDB;
 
 CREATE TABLE review (
 reviewID INT(10) AUTO_INCREMENT,
+userReviewing INT(10) NOT NULL,
+userReviewer INT(10) NOT NULL,
 reviewTime TIME NOT NULL,
 reviewDate DATE NOT NULL,
 starRating INT(1) NOT NUll,
 comment VARCHAR(256),
+FOREIGN KEY userReviewing REFERENCES user(userID),
+FOREIGN KEY userReviewer REFERENCES user(userID),
 PRIMARY KEY (ReviewID)
 )
 ENGINE=InnoDB;
 
 CREATE TABLE message (
 messageID INT(10) AUTO_INCREMENT,
-messageTime TIME NOT NULL,
-messageDate DATE NOT NULL,
+messageSentTime TIME NOT NULL,
+messageSentDate DATE NOT NULL,
+messageReadTime TIME NOT NULL,
+messageReadDate DATE NOT NULL,
 messageContent VARCHAR(1024) NOT NULL,
 PRIMARY KEY (messageID)
 )
@@ -42,10 +49,12 @@ ENGINE=InnoDB;
 
 CREATE TABLE notification (
 notificationID INT(10) AUTO_INCREMENT,
+userID INT(10) NOT NULL,
 notificationTime TIME NOT NULL,
 notificationDate DATE NOT NULL,
 notificationType VARCHAR(128) NOT NULL,
 notificationContent VARCHAR(256) NOT NULL,
+FOREIGN KEY userID REFERENCES user(userID),
 PRIMARY KEY (notificationID)
 )
 ENGINE=InnoDB;
@@ -62,23 +71,13 @@ PRIMARY KEY (reportID)
 )
 ENGINE=InnoDB;
 
-CREATE TABLE payment (
-paymentID INT(10) AUTO_INCREMENT,
-paymentTime TIME NOT NULL,
-paymentDate DATE NOT NULL,
-paymentAmount INT(6) NOT NULL,
-paymentStatus VARCHAR(1),
-PRIMARY KEY (paymentID)
-)
-ENGINE=InnoDB;
-
 CREATE TABLE loan (
 loanID INT(10) AUTO_INCREMENT,
 loanAmount INT(6) NOT NULL,
 interestRate DECIMAL(6, 6) NOT NULL,
 loanStatus VARCHAR(32) NOT NULL,
 loanDateStart DATE NOT NULL,
-paymentSchedule VARCHAR(12) NOT NULL,
+paymentSchedule VARCHAR(16) NOT NULL,
 dateSent DATE NOT NULL,
 timeSent TIME NOT NULL,
 dateAccepted DATE,
@@ -87,5 +86,59 @@ senderSignature VARCHAR(128) NOT NULL,
 receiverSignature VARCHAR(128) NOT NUll,
 groupLoanID INT(10),
 PRIMARY KEY (loanID)
+)
+ENGINE=InnoDB;
+
+CREATE TABLE payment (
+paymentID INT(10) AUTO_INCREMENT,
+loanID INT(10) NOT NULL,
+paymentTime TIME NOT NULL,
+paymentDate DATE NOT NULL,
+paymentAmount INT(6) NOT NULL,
+paymentStatus VARCHAR(16),
+FOREIGN KEY loanID REFERENCES loan(loanID),
+PRIMARY KEY (paymentID)
+)
+ENGINE=InnoDB;
+
+
+/*Connections linking strong Entities*/
+CREATE TABLE reportForm (
+userReporting INT(10) NOT NULL,
+userReported INT(10) NOT NULL,
+reportID INT(10) NOT NULL,
+FOREIGN KEY userReporting REFERENCES user(userID),
+FOREIGN KEY userReported REFERENCES user(userID),
+FOREIGN KEY reportID REFERENCES report(reportID)
+)
+ENGINE=InnoDB;
+
+CREATE TABLE userMessage (
+userSent INT(10) NOT NULL,
+userRecieve INT(10) NOT NULL,
+messageID INT(10) NOT NULL,
+FOREIGN KEY userSent REFERENCES user(userID),
+FOREIGN KEY userRecieve REFERENCES user(userID),
+FOREIGN KEY messageID REFERENCES message(messageID)
+)
+ENGINE=InnoDB;
+
+CREATE TABLE userPayment (
+userTo INT(10) NOT NULL,
+userFrom INT(10) NOT NULL,
+paymentID INT(10) NOT NULL,
+FOREIGN KEY userTo REFERENCES user(userID),
+FOREIGN KEY userFrom REFERENCES user(userID),
+FOREIGN KEY paymentID REFERENCES payment(paymentID)
+)
+ENGINE=InnoDB;
+
+CREATE TABLE userLoan (
+userSender INT(10) NOT NULL,
+userReciever INT(10) NOT NULL,
+loanID INT(10) NOT NULL,
+FOREIGN KEY userSender REFERENCES user(userID),
+FOREIGN KEY userReciever REFERENCES user(userID),
+FOREIGN KEY loanID REFERENCES loan(loanID)
 )
 ENGINE=InnoDB;
