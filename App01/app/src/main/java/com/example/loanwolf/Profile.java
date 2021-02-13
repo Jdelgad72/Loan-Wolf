e719b8645d01f9e3a7bff0f89117e6cedcd32e35
lugod <lugod@iu.edu>
2021-02-13 16:14:36 -0500

    Fixed some issues with the background on the Deposit/Withdraw screen.

--- App01/app/src/main/java/com/example/loanwolf/Profile.java
+++ App01/app/src/main/java/com/example/loanwolf/Profile.java
@@ -1,9 +1,15 @@
 package com.example.loanwolf;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Profile extends AppCompatActivity {
+
+    Button buttonDepositWithdraw, buttonHome, buttonProfile, buttonMessage;
+    TextView account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
