f4b06eb4b113b8d603c6841d0ff294730499e078
lugod <lugod@iu.edu>
2021-02-13 19:22:23 -0500

    Buttons on transaction activity now display/function on screen.

--- App01/app/src/main/java/com/example/loanwolf/depositWithdraw.java
+++ App01/app/src/main/java/com/example/loanwolf/depositWithdraw.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 public class depositWithdraw extends AppCompatActivity {
     Button button0, button01, button02, button03, button04, button05, button06, button07, button08, button09, buttonDeposit, buttonWithdraw, buttonPeriod, buttonClear, buttonBack;
     TextView workspace, message;
+    String add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +34,90 @@ public class depositWithdraw extends AppCompatActivity {
         buttonClear = findViewById(R.id.buttonClear);
         workspace = findViewById(R.id.workspace);
         message = findViewById(R.id.message);
+
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add = workspace.getText().toString();
+                workspace.setText(add + "0");
+            }
+        });
+        button01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add = workspace.getText().toString();
+                workspace.setText(add + "1");
+            }
+        });
+        button02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add = workspace.getText().toString();
+                workspace.setText(add + "2");
+            }
+        });
+        button03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add = workspace.getText().toString();
+                workspace.setText(add + "3");
+            }
+        });
+        button04.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add = workspace.getText().toString();
+                workspace.setText(add + "4");
+            }
+        });
+        button05.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add = workspace.getText().toString();
+                workspace.setText(add + "5");
+            }
+        });
+        button06.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add = workspace.getText().toString();
+                workspace.setText(add + "6");
+            }
+        });
+        button07.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add = workspace.getText().toString();
+                workspace.setText(add + "7");
+            }
+        });
+        button08.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add = workspace.getText().toString();
+                workspace.setText(add + "8");
+            }
+        });
+        button09.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add = workspace.getText().toString();
+                workspace.setText(add + "9");
+            }
+        });
+        buttonPeriod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add = workspace.getText().toString();
+                workspace.setText(add + ".");
+            }
+        });
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                workspace.setText("");
+            }
+        });
 
         backButton();
     }
