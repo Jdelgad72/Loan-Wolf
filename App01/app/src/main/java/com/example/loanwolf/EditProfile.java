f80599e7125875c4ce3baa6e28e258262ffb4bfd
samjawor <samjawor@iu.edu>
2021-02-14 20:30:21 -0500

    First work on Button

--- App01/app/src/main/java/com/example/loanwolf/EditProfile.java
+++ App01/app/src/main/java/com/example/loanwolf/EditProfile.java
@@ -3,6 +3,7 @@ package com.example.loanwolf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -32,5 +33,20 @@ public class EditProfile extends AppCompatActivity {
         Gender = (EditText) findViewById(R.id.editTextGender);
         Email = (EditText) findViewById(R.id.editTextEmail);
         Submit = (Button) findViewById(R.id.btnSubmit);
+        
+
+        Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(First.getText().toString().trim().length()==0|| Last.getText().toString().trim().length()==0)
+                    DOB.getText().toString().trim().length()==0 || Address.getText().toString().trim().length()==0)
+                    || ZIP.getText().toString().trim().length()==0) || State.getText().toString().trim().length()==0)
+                    || Gender.getText().toString().trim().length()==0) || Email.getText().toString().trim().length()==0)
+                {
+                    showMessage("Error", "Please do not leave any blank Fields");
+                    return;
+                }
+            }
+        });
     }
 }
