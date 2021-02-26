1ff125a84336b7f038a91927f2412dc50c079471
unknown <jdelgad@iu.edu>
2021-02-26 17:58:29 -0500

    Open dialog builder

--- App01/app/src/main/java/com/example/loanwolf/LoanAgreement.java
+++ App01/app/src/main/java/com/example/loanwolf/LoanAgreement.java
@@ -1,14 +1,28 @@
 package com.example.loanwolf;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoanAgreement extends AppCompatActivity {
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loan_agreement);
+
+        button2 = (Button)findViewById(R.id.button);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+    }
+    public void openDialog() {
+        
     }
 }
