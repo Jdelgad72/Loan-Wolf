04423f19979ddb85c733abd98dc8fa2d0a3fc337
samjawor <samjawor@iu.edu>
2021-02-28 00:00:02 -0500

    started making the widgets in NewReview.java

--- App01/app/src/main/java/com/example/loanwolf/NewReview.java
+++ App01/app/src/main/java/com/example/loanwolf/NewReview.java
@@ -3,8 +3,22 @@ package com.example.loanwolf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class NewReview extends AppCompatActivity {
+
+    private EditText Review;
+    private android.widget.RadioGroup RadioGroup;
+    private RadioButton One;
+    private RadioButton Two;
+    private RadioButton Three;
+    private RadioButton Four;
+    private RadioButton Five;
+    private Button Return;
+    private Button Submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
