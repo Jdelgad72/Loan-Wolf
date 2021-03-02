2029fcc0ac9781f1a60156d5d418e27b099684b5
samjawor <samjawor@iu.edu>
2021-03-01 19:54:58 -0500

    Added code to new review for posting to the database

--- App01/app/src/main/java/com/example/loanwolf/NewReview.java
+++ App01/app/src/main/java/com/example/loanwolf/NewReview.java
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Scroller;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class NewReview extends AppCompatActivity {
 
@@ -29,6 +39,11 @@ public class NewReview extends AppCompatActivity {
         Rating = (EditText) findViewById(R.id.editRating);
         Return = (Button) findViewById(R.id.btnReturn);
         Submit = (Button) findViewById(R.id.btnSubmit);
+
+        //get ID
+        User user = SharedPrefManager.getInfo();
+        final String id = user.getId();
+        final String email = user.getEmail();
 
         //Code so the EditText can scroll
         Review.setScroller(new Scroller(getApplicationContext()));
@@ -53,6 +68,56 @@ public class NewReview extends AppCompatActivity {
                     toast.show();
                     return;
                 }
+                User user = new User(
+                        id,
+                        email,
+                        Review.getText().toString().trim(),
+                        Rating.getText().toString().trim()
+                );
+
+                //storing the user in shared preferences
+                SharedPrefManager.getInstance(getApplicationContext()).userLogin(user);
+                // send the updated info to server and validates
+                String postUrl = "https://cgi.sice.indiana.edu/~team21/team-21/backend/newReview.php";
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, postUrl, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            //converting response to json object
+                            JSONObject obj = new JSONObject(response);
+
+                            //if no error in response
+                            if (!obj.getBoolean("error")) {
+                                Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+                                Log.d("RESPONSE1", obj.getString("message"));
+                            } else {
+                                Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+                                Log.d("RESPONSE2", obj.getString("message"));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.d("RESPONSE1", String.valueOf(e));
+                        }
+                    }
+                },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("VolleyError", String.valueOf(error));
+                            }
+                        }) {
+                    //updated user information Sent
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("id", id);
+                        params.put("review", Review.getText().toString().trim());
+                        params.put("rating", Rating.getText().toString().trim());
+                        return params;
+                    }
+                };
+                VolleySingleton.getInstance(EditProfile.this).addToRequestQueue(stringRequest);
             }//end public void for onclick listener
         });//end submit onclick listener
     }//end on create
