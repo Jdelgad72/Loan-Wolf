a3446563250c33ad81a56d82c7dcde6a53fb7d07
lugod <lugod@iu.edu>
2021-03-02 14:38:17 -0500

    Added 3 acivities, Joining wolfpack, creating wolfpack, and viewing wolfpacks, along with titles in the activities.

---
+++ App01/app/src/main/java/com/example/loanwolf/JoinWolfPack.java
@@ -0,0 +1,14 @@
+package com.example.loanwolf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JoinWolfPack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_wolf_pack);
+    }
+}
---
+++ App01/app/src/main/java/com/example/loanwolf/ViewWolfPacks.java
@@ -0,0 +1,14 @@
+package com.example.loanwolf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewWolfPacks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_wolf_packs);
+    }
+}
