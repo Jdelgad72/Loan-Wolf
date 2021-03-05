efb9d4c079c83bd38a370c2e64637160ffbf6cfc
Justin Delgado <jdelgad@iu.edu>
2021-03-05 18:47:26 -0500

    Backend engineering

--- backend/loanAgreement.php
+++ backend/loanAgreement.php
@@ -1,73 +1,32 @@
+
 <?php
-//Submitter.php
-//This submitter class encapsules the name, emailID, Value and Interest rate
-//Of the submitter
+require_once '../../../vendor/autoload.php';
+require_once 'connection.php';
 
-class Submitter {
-var $name;
-var $emailId
-var $value
-Var $rate
 
-// public constructor
-function Submitter($name, $emailId, $value, $rate) {
-$this->name = $name;
-$this->emailId = $emailId;
-$this->value = $value;
-$this->rate = $rate;
-}
+$response = array();
+if(isset($_POST['idToken'])){
 
-//Public method marshals object into xml
-function toXml() {
-if (!isset($this->submitterXml)) {
-$submitterXml = '<name>' . $this->getName(). '</name>';
-$submitterXml = $submitterXml . '<email>' . $this->getEMailId() . '</email>';
-$submitterXml = $submitterXml . '<value>' . $this->getValue() . '</value>';
-$submitterXml = $submitterXml . '<rate>' . $this->getRate() . '</rate>';
 
-$this->submitterXml = %submitterXml;
-}
-return $this->submitterXml;
-}
+      //inserts the id, name, value and rate.
+      $stmt = $conn->prepare("INSERT INTO user(firstName, lastName, email, accountGoogle, amount) VALUES (?, ?, ?, ?, 0)");
+      $stmt->bind_param("ssss", $firstName, $lastName, $email, $userid);
+      if($stmt->execute()){
+	$response['error'] = false;
+        $response['newUser'] = true;
+        $response['message'] = 'User registered successfully';
+        $response['user'] = $user;
 
-//public get/set functions for this class' data members
-function setName($name) {
-$this->name = $name;
-}
+      );
 
-function getName() {
-return $this->name;
-}
+      
 
-function setEMailId($emailId) {
-$this->emailId = $emailId;
-}
-
-function getEMailId() {
-return $this->emailId;
-}
-
-function setValue($value) {
-$this->value = $value;
-}
-
-function getValue($value) {
-return $this->value;
-}
-
-function setRate($rate) {
-$this->rate = $rate;
-}
-
-function getRate($rate) {
-return $this = $rate;
-}
+}else{
+  $response['error']=true;
+  $response['message']='Missing Id Token';
+}  
+echo json_encode($response);
 ?>
-
-
-
-
-
 
 
 
