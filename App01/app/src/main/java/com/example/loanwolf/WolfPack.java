a2e660554c3102f8ff037a3aef10474e440ef886
lugod <lugod@iu.edu>
2021-03-04 20:28:56 -0500

    Added new button to home screen to go to the wolfpack main activity along with creating the intent to go to the wolfpack main activity to view wolfpacks activity.

--- App01/app/src/main/java/com/example/loanwolf/WolfPack.java
+++ App01/app/src/main/java/com/example/loanwolf/WolfPack.java
@@ -2,7 +2,9 @@ package com.example.loanwolf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class WolfPack extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class WolfPack extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wolf_pack);
+    }
+
+    public void Buttonclick(View view) {
+        startActivity(new Intent(WolfPack.this, ViewWolfPacks.class));
     }
 }
