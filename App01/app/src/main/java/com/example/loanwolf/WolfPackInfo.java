bb544f6574aa18d21334fa0d481499d608c42c27
lugod <lugod@iu.edu>
2021-03-04 21:43:03 -0500

    Created new activity for wolf pack info and added a title for it.

---
+++ App01/app/src/main/java/com/example/loanwolf/WolfPackInfo.java
@@ -0,0 +1,14 @@
+package com.example.loanwolf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WolfPackInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wolf_pack_info);
+    }
+}
