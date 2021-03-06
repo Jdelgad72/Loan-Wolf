2330d98509cff4846e870f747881cfb2dd5f5150
Justin Delgado <jdelgad@iu.edu>
2021-03-05 22:06:26 -0500

    php for loan agreement

--- backend/loanAgreement.php
+++ backend/loanAgreement.php
@@ -1,16 +1,26 @@
 
 <?php
-require_once '../../../vendor/autoload.php';
+
 require_once 'connection.php';
 
 
 $response = array();
-if(isset($_POST['idToken'])){
+if(isset($_POST['RADIO'])){
+$radio = $_POST['RADIO'];
+$radio2 = $_POST['RADIO2'];
+$username = $_POST['USERNAME'];
+$email = $_POST['EMAIL'];
+$date = $_POST['DATE'];
+$num = $_POST['PAYMENTNUM'];
+$value = $_POST['VALUE'];
+$rate = $_POST['RATE'];
+
 
 
       //inserts the id, name, value and rate.
-      $stmt = $conn->prepare("INSERT INTO user(firstName, lastName, email, accountGoogle, amount) VALUES (?, ?, ?, ?, 0)");
-      $stmt->bind_param("ssss", $firstName, $lastName, $email, $userid);
+     $stmt = $conn->prepare("INSERT INTO loan(loanAmount, interestRate, loanStatus, notificationStatus, loanDateStart, loanDateEnd, paymentSchedule, numberPayments, dateSent, timeSent, creditorSignature) VALUES (?, ?, 'pending' , 'Sent' , ?, '2021-08-01', ?, ?, CURDATE(), CURRENT_TIME(), ?);";
+      $stmt->bind_param("iissis", $value, $rate, $date, $radio2, $num, $username );
+
       if($stmt->execute()){
 	$response['error'] = false;
         $response['newUser'] = true;
