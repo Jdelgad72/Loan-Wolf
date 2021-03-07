d931b757bcee180d40adb1d26ba4ade470d1de04
samjawor <samjawor@iu.edu>
2021-03-07 17:56:29 -0500

    Added a return button to ViewReviews.java, goes to ViewProfile

--- App01/app/src/main/java/com/example/loanwolf/ViewReviews.java
+++ App01/app/src/main/java/com/example/loanwolf/ViewReviews.java
@@ -2,13 +2,28 @@ package com.example.loanwolf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ViewReviews extends AppCompatActivity {
+
+    private Button Return;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_reviews);
+
+        Return = (Button) findViewById(R.id.btnReturn);
+
+        //return to the profile page without changing anything
+        Return.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ViewReviews.this, ViewProfile.class));
+            }
+        });
     }
 }
