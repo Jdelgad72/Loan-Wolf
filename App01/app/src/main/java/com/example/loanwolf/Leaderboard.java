95bdaf626c311d7ac9ab4febd908c73016b3f34c
samjawor <samjawor@iu.edu>
2021-03-08 23:07:18 -0500

    Added some code to Leaderboard.java

--- App01/app/src/main/java/com/example/loanwolf/Leaderboard.java
+++ App01/app/src/main/java/com/example/loanwolf/Leaderboard.java
@@ -2,13 +2,42 @@ package com.example.loanwolf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Leaderboard extends AppCompatActivity {
+
+    private Button Return;
+    private android.widget.ListView ListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
+
+        Return = (Button) findViewById(R.id.btnReturn);
+        ListView = (ListView) findViewById(R.id.listview);
+
+        ArrayList<String> arrayList=new ArrayList<>();
+
+        arrayList.add("Sample Text");
+
+        //create array adapter
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
+        ListView.setAdapter(arrayAdapter);
+
+        //return to the profile page without changing anything
+        Return.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Leaderboard.this, ViewProfile.class));
+            }
+        });
     }
 }
