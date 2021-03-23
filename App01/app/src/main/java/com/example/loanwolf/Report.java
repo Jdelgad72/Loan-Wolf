a7f293dc68e57b10c6b4e21506da00a649aebdfb
unknown <jdelgad@iu.edu>
2021-03-23 19:01:34 -0400

    Report user feature completed

--- App01/app/src/main/java/com/example/loanwolf/Report.java
+++ App01/app/src/main/java/com/example/loanwolf/Report.java
@@ -2,13 +2,48 @@ package com.example.loanwolf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Report extends AppCompatActivity {
+    EditText et;
+    Button btn;
+    final String name = getIntent().getStringExtra("USERNAME");
+    final String email = getIntent().getStringExtra("EMAIL");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+        et = findViewById(R.id.editTextTextMultiLine);
+        btn = findViewById(R.id.button3);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(et.getText().toString().isEmpty() || et.getText().toString()==null) {
+                    Toast.makeText(getApplicationContext(), "User Reported", Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(getApplicationContext(), "Report Unsuccessful", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Report.this, ViewProfile.class);
+                intent.putExtra("USERNAME", name);
+                intent.putExtra("EMAIL", email);
+                startActivity(intent);
+            }
+        });
+
     }
 }
