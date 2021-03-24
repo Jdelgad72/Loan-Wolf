4b81fa6a1bd63726ffeeece1ab91a7c264ea3cde
samjawor <samjawor@iu.edu>
2021-03-24 18:13:50 -0400

    Wrote code for LeaderboardListObject

--- App01/app/src/main/java/com/example/loanwolf/LeaderboardListObject.java
+++ App01/app/src/main/java/com/example/loanwolf/LeaderboardListObject.java
@@ -1,4 +1,32 @@
 package com.example.loanwolf;
 
 public class LeaderboardListObject {
+    private String rank;
+    private String name;
+    private String email;
+    private String starRating;
+
+    public LeaderboardListObject(String rank, String name, String email, String starRating) {
+        this.rank = rank;
+        this.name = name;
+        this.email = email;
+        this.starRating = starRating;
+    }
+
+    public String getrank() {
+        return this.rank;
+    }
+
+    public String getname() {
+        return this.name;
+    }
+
+    public String getemail() {
+        return this.email;
+    }
+
+    public String getstarRating() {
+        return this.starRating;
+    }
+
 }
