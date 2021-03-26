c1f3931d1924568b8b794d94fece0c8eadaff119
lugod <lugod@iu.edu>
2021-03-26 14:28:45 -0400

    Created new page for wolf pack to get amount and intrest rate from user also connected a button from the wolf pack screen to get to the activity.

---
+++ App01/app/src/main/java/com/example/loanwolf/wolfPackOptions.java
@@ -0,0 +1,19 @@
+package com.example.loanwolf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class wolfPackOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wolf_pack_options);
+    }
+    public void buttonCreate(View view) {
+        startActivity(new Intent(wolfPackOptions.this, CreateWolfPack.class));
+    }
+}
