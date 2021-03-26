e3dfd42c1a20abb7cde5800de6a40affa3786b84
lugod <lugod@iu.edu>
2021-03-26 11:46:05 -0400

    Created an activity for Payment Schedule along with adding a title in it, additionally discovered in some errors in java leaderboard file

---
+++ App01/app/src/main/java/com/example/loanwolf/ViewPaymentSchedule.java
@@ -0,0 +1,14 @@
+package com.example.loanwolf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewPaymentSchedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_payment_schedule);
+    }
+}
