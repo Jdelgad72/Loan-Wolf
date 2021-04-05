b17e78e6255601d1235fa480767e59bce84ace77
Felipe Garcilazo <feligarc@iu.edu>
2021-04-04 21:14:28 -0400

    Adding Backend for messaging and conversation features.

--- backend/conversation.php
+++ backend/conversation.php
@@ -2,31 +2,29 @@
 require_once 'connection.php';
 
 $response = array();
-if(isset($_POST['ID'])){
+if(isset($_POST['ID'], $_POST['email'])){
 
 $id = $_POST['ID'];
+$email = $_POST['email'];
 
-$names = array();
-$emails = array();
+$statuses = array();
 $messages = array();
 $dates = array();
 $times = array();
 
-$sql ="SELECT CONCAT(u.firstName, ' ', u.lastName) AS name, u.email, m.messageContent, m.messageSentTime, m.messageSentDate FROM user AS u, userMessage AS um, message AS m WHERE um.messageID=m.messageID AND ((um.userSent=u.userID AND um.userRecieve=$id) OR (um.userRecieve=u.userID AND um.userSent=$id)) GROUP BY u.email ORDER BY m.messageSentDate ASC, m.messageSentTime ASC;";
+$sql ="SELECT * FROM (SELECT 'Sender' AS status, m.messageContent, m.messageSentTime, m.messageSentDate FROM user AS u, userMessage AS um, message AS m WHERE um.messageID=m.messageID AND (um.userRecieve=u.userID AND um.userSent=$id) AND u.email='$email' UNION SELECT 'Reciever' AS status, m.messageContent, m.messageSentTime, m.messageSentDate FROM user AS u, userMessage AS um, message AS m WHERE um.messageID=m.messageID AND (um.userSent=u.userID AND um.userRecieve=$id) AND u.email='$email') AS a ORDER BY a.messageSentDate DESC, a.messageSentTime DESC;";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
 
   while($row = mysqli_fetch_assoc($result)){
-    array_push($names, $row["name"]);
-    array_push($emails, $row["email"]);
+    array_push($statuses, $row["status"]);
     array_push($messages, $row["messageContent"]);
     array_push($dates, $row["messageSentDate"]);
     array_push($times, $row["messageSentTime"]);
   }
 
-  $response["names"] = $names;
-  $response["emails"] = $emails;
+  $response["statuses"] = $statuses;
   $response["messages"] = $messages;
   $response["dates"] = $dates;
   $response["times"] = $times;
