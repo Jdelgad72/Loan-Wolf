1422ba3b6ddc6542328ba1568d8cd0f11187ea2e
lugod <lugod@iu.edu>
2021-04-10 12:02:58 -0400

    Fixed mutiple issues with problems that occured in opening loans additionally futher working on information for weekly monthly, and daily payment acitivities.

---
+++ App01/app/src/main/java/com/example/loanwolf/ViewMonthlySchedule.java
@@ -0,0 +1,14 @@
+package com.example.loanwolf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewMonthlySchedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_monthly_schedule);
+    }
+}
--- App01/app/src/main/java/com/example/loanwolf/notificationTest.java
+++ App01/app/src/main/java/com/example/loanwolf/notificationTest.java
@@ -2,7 +2,9 @@ package com.example.loanwolf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class notificationTest extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class notificationTest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_test);
+    }
+    public void buttonDaily(View view) {
+        startActivity(new Intent(notificationTest.this, ViewPaymentSchedule.class));
+    }
+    public void buttonWeekly(View view) {
+        startActivity(new Intent(notificationTest.this, ViewWeeklySchedule.class));
+    }
+    public void buttonMonthly(View view) {
+        startActivity(new Intent(notificationTest.this, ViewMonthlySchedule.class));
     }
 }
